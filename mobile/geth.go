04db5fdb504e6731ab6476c4ee36d4ffa93380e6
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-03-20 14:27:45 -0700

    update network id

--- mobile/geth.go
+++ mobile/geth.go
@@ -80,7 +80,7 @@ var defaultNodeConfig = &NodeConfig{
 	BootstrapNodes:        FoundationBootnodes(),
 	MaxPeers:              25,
 	EthereumEnabled:       true,
-	EthereumNetworkID:     1,
+	EthereumNetworkID:     33666,
 	EthereumDatabaseCache: 16,
 }
 
