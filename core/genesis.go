98cb0383d1382d9e5218761788638ecab477cee0
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-03-23 16:26:17 -0700

    hi oz!

--- core/genesis.go
+++ core/genesis.go
@@ -321,7 +321,7 @@ func DefaultGenesisBlock() *Genesis {
 		//190019876660
 		//2129553840825
 		//4529553840825
-		Difficulty: big.NewInt(2129553),
+		Difficulty: big.NewInt(152239320),
 		Alloc:      decodePrealloc(mainnetAllocData),
 	}
 }
